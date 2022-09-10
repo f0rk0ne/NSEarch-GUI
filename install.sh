25b14d76d3bcb262ccec1eb8cd4382ee4b4a0eaa
f0rk0ne <77067446+f0rk0ne@users.noreply.github.com>
2022-09-09 21:41:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ function installpipDebian(){
 
 function installpipRedHat(){
   printf "[+] Installing pip ...\n"
-  rpm -iUvh rpm -iUvh https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-14.noarch.rpm; yum -y update; yum -y update
+  rpm -iUvh https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-14.noarch.rpm; yum -y update; yum -y update
   yum install python3-pip3 -y
   installPipRequeriments
 }
